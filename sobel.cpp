18d0d2572701dceab9075061c90b5b3e849431bb
heung8198 <107354972+heung8198@users.noreply.github.com>
2023-11-22 13:06:32 +0900

    Create sobel.cpp

---
+++ sobel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/photo.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
